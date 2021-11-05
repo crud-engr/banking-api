7bff67b6b59d2b6eba12c0a510026d4d1242b889
Abeeb Ayinla <abeebayinla@gmail>
2021-11-06 00:00:06 +0100

    removed get user decorator

--- src/auth/get-user.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { createParamDecorator } from "@nestjs/common";
-import { User } from "src/users/schemas/users.schemas";
-
-export const GetUser = createParamDecorator((data, req): User => {
-    return req.user;
-});
