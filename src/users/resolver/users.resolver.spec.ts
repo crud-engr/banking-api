baa975ebb9a2e038f63c55d55587025295fb3a85
Abeeb Ayinla <abeebayinla@gmail>
2021-11-05 23:47:29 +0100

    prepare user resolver for testing

--- src/users/resolver/users.resolver.spec.ts
+++ src/users/resolver/users.resolver.spec.ts
@@ -1,13 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../service/users.service';
 import { UsersResolver } from './users.resolver';
 
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
 
+  const mockUsersService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver],
-    }).compile();
+      providers: [UsersResolver, UsersService],
+    }).overrideProvider(UsersService).useValue(mockUsersService).compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
   });
